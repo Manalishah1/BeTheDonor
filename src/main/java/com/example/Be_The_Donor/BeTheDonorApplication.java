391925a3ab89cc3cce90dfee61488590b9926fee
dhson <dh657288@dal.ca>
2022-02-25 17:57:13 -0400

    Project Structure Created

---
+++ src/main/java/com/example/Be_The_Donor/BeTheDonorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Be_The_Donor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTheDonorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTheDonorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Be_The_Donor/BeTheDonorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Be_The_Donor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTheDonorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
