899b77adf00b6ad9783c9914e6d68956e48626bc
praval <pr647062@dal.ca>
2022-03-08 01:03:54 -0400

    JWT Token and Session management

--- src/main/java/com/example/Be_The_Donor/controller/UserLoginController.java
+++ src/main/java/com/example/Be_The_Donor/controller/UserLoginController.java
@@ -2,21 +2,29 @@ package com.example.Be_The_Donor.controller;
 
 import com.example.Be_The_Donor.config.PasswordEncoder;
 import com.example.Be_The_Donor.entity.ApplicationUser;
+import com.example.Be_The_Donor.security.JwtTokenUtil;
 import com.example.Be_The_Donor.service.ApplicationUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@AllArgsConstructor
 public class UserLoginController {
 
     @Autowired
@@ -25,24 +33,33 @@ public class UserLoginController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @RequestMapping(value = "/Test", method = RequestMethod.POST)
+
+    @Autowired
+    private DaoAuthenticationProvider authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private ApplicationUserService userDetailsService;
+
+  /*  @RequestMapping(value = "/Test", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, Object> getUser(HttpServletResponse httpServletResponse , @RequestBody ApplicationUser user) {
+    public Map<String, Object> getUser(HttpServletResponse httpServletResponse, @RequestBody ApplicationUser user) {
         Map<String, Object> response = new HashMap<String, Object>();
         try {
 
 
             ApplicationUser applicationUser = applicationUserService.findByEmail(user.getEmail()).orElse(null);
 
-            boolean isPasswordMatch = passwordEncoder.bCryptPasswordEncoder().matches(user.getPassword(),applicationUser.getPassword());
-            if(isPasswordMatch) {
+            boolean isPasswordMatch = passwordEncoder.bCryptPasswordEncoder().matches(user.getPassword(), applicationUser.getPassword());
+            if (isPasswordMatch) {
                 System.out.println("check");
                 if (applicationUser.getEmail().equals(user.getEmail())) {
                     response.put("data", "Login Successfully Done");
                     httpServletResponse.setStatus(HttpServletResponse.SC_CREATED);
                 }
-            }
-            else{
+            } else {
                 System.out.println("incorrect username or password");
             }
 
@@ -50,30 +67,36 @@ public class UserLoginController {
             response.put("error", ex.getMessage());
         }
         return response;
+    }*/
+
+    @GetMapping("/loginSuccess")
+    public String viewLoginPage() {
+        // custom logic before showing login page...
+
+        return "loginSuccess";
     }
 
-
-    @RequestMapping(value = "/loginSuccess", method = RequestMethod.GET)
-    @ResponseBody
-    public Map<String, Object> checkAuthentication() {
+    @PostMapping("/loginSuccess")
+    public String checkAuthentication() {
         Map<String, Object> response = new HashMap<String, Object>();
         try {
-            response.put("data", "Login Successful for USER");
+            response.put("data", "Login Successful");
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             response.put("error", ex.getMessage());
         }
-        return response;
+        return "redirect:/loginSuccess";
     }
+
 
     @RequestMapping(value = "/loginSuccess1", method = RequestMethod.GET)
     @ResponseBody
     public Map<String, Object> checkAuthentication1() {
         Map<String, Object> response = new HashMap<String, Object>();
         try {
-            response.put("data", "Login Successful FOR ADMIN");
+            response.put("data", "Login Successful1");
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             response.put("error", ex.getMessage());
         }
         return response;
@@ -86,7 +109,7 @@ public class UserLoginController {
         try {
             response.put("data", "Logout Successful");
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             response.put("error", ex.getMessage());
         }
         return response;
@@ -99,10 +122,43 @@ public class UserLoginController {
         try {
             response.put("data", "access denied");
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             response.put("error", ex.getMessage());
         }
         return response;
+    }
+
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(HttpServletRequest request, @RequestBody ApplicationUser user) throws Exception {
+        Authentication authentication = authenticate(user.getEmail(), user.getPassword());
+        // Principal principal = request.getUserPrincipal();
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(user.getEmail());
+        applicationUserService.loadUserByUsername(user.getEmail());
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        // Create a new session and add the security context.
+        HttpSession session = request.getSession(true);
+        session.setAttribute(user.getEmail(), securityContext);
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(token);
+    }
+
+
+    private Authentication authenticate(String username, String password) throws Exception {
+        try {
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
 }
