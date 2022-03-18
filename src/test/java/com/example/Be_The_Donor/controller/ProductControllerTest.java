a7932724387b56a47b1d4b489dcfd5beb2370a5e
jayashree <jy357640@dal.ca>
2022-03-18 16:24:18 +0530

    added changes

--- src/test/java/com/example/Be_The_Donor/controller/ProductControllerTest.java
+++ src/test/java/com/example/Be_The_Donor/controller/ProductControllerTest.java
@@ -23,47 +23,52 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import com.example.Be_The_Donor.entity.Product;
 import com.example.Be_The_Donor.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+// To start the application context and to detect the start up issues
 @SpringBootTest
 public class ProductControllerTest {
-
+	// creating a mock object of the class ProductRepository
 	@Mock
 	private ProductService productService;
 
 	private ObjectMapper mapper = new ObjectMapper();
 	
 	private MockMvc mockMvc;
-	
+	// calling junit - injecting the mock object in ProductController class
 	@InjectMocks
 	ProductController controller;
-
+// This method to be executed before each invocation of testUpdateProducts
     @BeforeEach
     public void setup() {
+//    	Receiving the controller objects as parameters, building MOCKMvc instance by registering the controller instance
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
+//    Creating mock product with the name "Dummy", with price 200 and with quantity 5
 	@Test
 	public void testUpdateProducts() throws Exception {
 		Product product = new Product();
 		product.setProductName("Dummy");
 		product.setPrice(200.0d);
 		product.setQuantity(5);
-		
+//		Checking with dummy data, passing any instance of product, if exists, then returning the product
 		when(productService.addQuantity(Mockito.any(Product.class))).thenReturn(product);
-
+//      Creating request builder to make request
+//		mapper converts JSON object to JSON String
+//		Calling API
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/v1/updateProducts").content(mapper.writeValueAsString(product))
 				.contentType(MediaType.APPLICATION_JSON);
-
+//  API value is got
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
+// Sending dummy product to API
+//		Getting the response as JSON String  and converting it to JSON
 		Product prdctResponse = mapper.readValue(result.getResponse().getContentAsString(), Product.class);
-
+// Checking with dummy data to see if it is not null, if it has a quantity
 		assertThat(prdctResponse).isNotNull();
 		assertEquals(prdctResponse.getProductName(), "Dummy");
 		assertEquals(prdctResponse.getQuantity(), 5);
 
 	}
-	
+	//    Creating mock product with the name "Dummy", with price 200 and with quantity 5
 	@Test
 	public void testGetProducts() throws Exception {
 		List<Product> listProduct = new ArrayList<Product>();
@@ -71,16 +76,19 @@ public class ProductControllerTest {
 		product.setProductName("Dummy");
 		product.setPrice(200.0d);
 		listProduct.add(product);
-		
-		when(productService.getProducts()).thenReturn(listProduct);
 
+//		Checking with dummy data, if able to get the products, then returning the list of products
+		when(productService.getProducts()).thenReturn(listProduct);
+//      Creating request builder to make request
+//		mapper converts JSON object to JSON String
+//		Calling API
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/getProducts")
 				.contentType(MediaType.APPLICATION_JSON);
-
+//  API value is got
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
+//		Getting the response as JSON String  and converting it to JSON
 		List<Product> prdctResponse = mapper.readValue(result.getResponse().getContentAsString(), List.class);
-
+// Checking with dummy data to see if it is not null, if it has a quantity
 		assertThat(prdctResponse).isNotNull();
 
 	}
--- src/test/java/com/example/Be_The_Donor/service/ProductServiceTest.java
+++ src/test/java/com/example/Be_The_Donor/service/ProductServiceTest.java
@@ -17,13 +17,13 @@ import com.example.Be_The_Donor.repository.ProductRepository;
 // To start the application context and to detect the start up issues 
 @SpringBootTest
 public class ProductServiceTest {
-// creating a mock object of the class
+// creating a mock object of the class ProductRepository
 	@Mock
 	private ProductRepository productRepository;
-
+// calling junit
 	@InjectMocks
 	ProductServiceImpl service;
-
+	//    Creating mock product with the name "Dummy", with price 200 and with quantity 5
 	@Test
 	public void testGetProduct() throws Exception {
 		List<Product> listproduct = new ArrayList<Product>();
@@ -32,27 +32,25 @@ public class ProductServiceTest {
 		product.setPrice(200.0d);
 		product.setQuantity(5);
 		listproduct.add(product);
-		
+//		Checking with dummy data,if it exists, then returning the list of products
 		when(productRepository.findAll()).thenReturn(listproduct);
-
+//      Checking with dummy data and checking to see if it not null and has at least 1 product
 		List<Product> productResp = service.getProducts();
 		assertThat(productResp).isNotNull();
 		assertThat(productResp).hasSize(1);
 
 	}
-	
+	//    Creating mock product with the name "Dummy", with price 200 and with quantity 5
 	@Test
 	public void testUpdateProduct() throws Exception {
-		
 		Product product = new Product();
 		product.setProductName("Dummy");
 		product.setPrice(200.0d);
 		product.setQuantity(5);
-		
+//		Checking with dummy data, not saving it in db, but returning the dummy product
 		when(productRepository.save(Mockito.any(Product.class))).thenReturn(product);
-
+//      Checking with dummy data for updating and checking to see if it not null and has at least 1 product
 		Product productResp = service.addQuantity(product);
-
 		assertThat(productResp).isNotNull();
 
 	}
