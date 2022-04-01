0fe545d9676949cdc268e7ec1e01494d51a71cdb
dhairya <dh263020@sal.ca>
2022-04-01 01:17:58 -0300

    product backend integration

--- src/main/java/com/example/Be_The_Donor/controller/AdminController.java
+++ src/main/java/com/example/Be_The_Donor/controller/AdminController.java
@@ -1,15 +1,24 @@
 package com.example.Be_The_Donor.controller;
 
+import com.example.Be_The_Donor.entity.Product;
+import com.example.Be_The_Donor.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class AdminController {
+    ProductService productService;
     @GetMapping("/api/v1/admin")
-    public String adminDashboard()
+    public String adminDashboard(Model model)
     {
+        List<Product> products = productService.getProducts();
+        model.addAttribute("products",products);
+
         return "admindashboard";
     }
 }
