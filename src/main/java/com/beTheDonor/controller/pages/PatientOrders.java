26d205549d25f2ce05a12025f01bc7cc49b2aad3
Manali Shah <mn749104@dal.ca>
2022-04-05 19:38:55 -0300

    Update PatientOrders.java

--- src/main/java/com/beTheDonor/controller/pages/PatientOrders.java
+++ src/main/java/com/beTheDonor/controller/pages/PatientOrders.java
@@ -3,11 +3,14 @@ package com.beTheDonor.controller.pages;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class PatientOrders {
-    @RequestMapping(value = "/patient/orders")
-    public String patientOrders() {
-        return "patientOrders";
+    @RequestMapping("/patient/orders")
+    public ModelAndView patientOrders() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("patientOrders.html");
+        return modelAndView;
     }
 }
